070e5c5a3a63becbe045d285ee15e4b0f483baa6
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-31 08:57:04 -0300

    Clear project

--- app_04/src/App.jsx
+++ app_04/src/App.jsx
@@ -1,13 +1,10 @@
-import Userbar from "./components/Userbar";
+
 
 export default function App() {
 
   return (
     <>
-      <h3>React Prop-Drilling</h3>
-
-      <Userbar />
-
+      <h1>React Múltiplos Context APIs</h1>
 
     </>
   );
--- app_04/src/index.js
+++ app_04/src/index.js
@@ -1,12 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-import { AuthProvider } from "./providers/AuthContext";
-
-createRoot(document.querySelector("#root")).render(
-    <AuthProvider>
-        <App />
-    </AuthProvider>
-
-);
+createRoot(document.querySelector("#root")).render(<App />);
 
